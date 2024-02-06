fb2b9443592358a0dfb411983e36ce09a545f0b9
Garbi93 <iseung2093@gmail.com>
2024-02-06 20:04:45 +0900

    02/06 @controller @service @repository 어노테이션을 사용 하여 spring 이 컴포넌트 스캥 하게 작동 시키고 @Autowired를 사용하여 의존성 주입

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     // 서비스는 용어를 비지니스에 가깝게 네이밍 해준다.
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
