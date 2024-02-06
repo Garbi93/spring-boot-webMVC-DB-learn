b7e47e41a1295c66919f064235ad72ac86c21c83
Garbi93 <iseung2093@gmail.com>
2024-02-06 18:19:52 +0900

    02/06 회원 서비스 테스트 코드 작성 임폴트 부분 정리

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,18 +1,12 @@
 package hello.hellospring.service;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class MemberServiceTest {
 
@@ -66,11 +60,7 @@ class MemberServiceTest {
 //        } catch (IllegalStateException e) {
 //            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원 입니다.");
 //        }
-
-
         // then
-
-
     }
 
 
